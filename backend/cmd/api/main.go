c8e5553b78d9814d03a114a935d49f3175538d16
dariatureac13 <dariatureac@gmail.com>
2025-09-11 09:40:13 +0300

    Add drone position log

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -8,13 +8,13 @@ import (
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 
+	"agro-bot/internal/db"
 	"agro-bot/internal/http/handler"
-	"agro-bot/internal/http/router"
 	"agro-bot/internal/http/middleware"
+	"agro-bot/internal/http/router"
+	"agro-bot/internal/mav"
 	"agro-bot/internal/ws"
-    "agro-bot/internal/mav"
 )
-
 
 func main() {
 	dsn := os.Getenv("DATABASE_URL")
@@ -22,48 +22,53 @@ func main() {
 		log.Fatal("DATABASE_URL not set")
 	}
 
-	db, err := sql.Open("pgx", dsn)
+	sqlDB, err := sql.Open("pgx", dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+	defer sqlDB.Close()
 
-    mavc, err := mav.New(mav.Options{
-        UDPAddr:         "0.0.0.0:14550",
-        OutSystemID:     255,
-        OutComponentID:  190,
-        TargetSystem:    1,
-        TargetComponent: 1,
-    })
+	mavc, err := mav.New(mav.Options{
+		UDPAddr:         "0.0.0.0:14550",
+		OutSystemID:     255,
+		OutComponentID:  190,
+		TargetSystem:    1,
+		TargetComponent: 1,
+	})
 
-    if err != nil {log.Fatal(err)}
-    mavc.OnPos = func(p ws.Pos) {
-        ws.DronePosBroadcast(ws.Pos{Lat: p.Lat, Lon: p.Lon});
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
+	mavc.OnPos = func(p ws.Pos) {
 
-    mavc.OnMissionReached = func(seq uint16) {
-        if mavc.MissionActive && mavc.LastSeq == seq {
-            log.Println("Mission completed")
-            mavc.MissionActive = false
-            mavc.LastSeq = 0
+		ws.DronePosBroadcast(ws.Pos{Lat: p.Lat, Lon: p.Lon})
 
-            ws.DroneMissionBroadcast(ws.MissionStatus{Status: true});
-        }
-    }
+		db.SaveIfChanged(sqlDB, p)
+	}
+
+	mavc.OnMissionReached = func(seq uint16) {
+		if mavc.MissionActive && mavc.LastSeq == seq {
+			log.Println("Mission completed")
+			mavc.MissionActive = false
+			mavc.LastSeq = 0
+
+			ws.DroneMissionBroadcast(ws.MissionStatus{Status: true})
+		}
+	}
 
 	mux := http.NewServeMux()
 
-    droneHandler := handler.NewDrone(mavc)
-    mux.HandleFunc("/drone/goto", droneHandler.Goto)
-    mux.HandleFunc("/drone/mission", droneHandler.Mission)
+	droneHandler := handler.NewDrone(mavc)
+	mux.HandleFunc("/drone/goto", droneHandler.Goto)
+	mux.HandleFunc("/drone/mission", droneHandler.Mission)
 
-	testHandler := handler.TestHandler{DB: db}
+	testHandler := handler.TestHandler{DB: sqlDB}
 	router.TestRouter(mux, testHandler)
 
-    mux.HandleFunc("/drone/position", ws.DronePosHandle)
-    mux.HandleFunc("/drone/mission/status", ws.DroneMissionHandle)
+	mux.HandleFunc("/drone/position", ws.DronePosHandle)
+	mux.HandleFunc("/drone/mission/status", ws.DroneMissionHandle)
 
-    handler := middleware.CorsMiddleware(mux)
+	handler := middleware.CorsMiddleware(mux)
 	log.Println("listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", handler))
 }
