59e7109572eb03bd8383c13a44954412b6e6f86d
dariatureac13 <dariatureac@gmail.com>
2025-09-17 12:52:09 +0300

    add button stop

--- backend/internal/http/router/drone.go
+++ backend/internal/http/router/drone.go
@@ -1,14 +1,15 @@
 package router
 
 import (
-	"net/http"
 	"agro-bot/internal/http/handler"
 	"agro-bot/internal/http/wshandler"
+	"net/http"
 )
 
 func DroneRouter(mux *http.ServeMux, handler *handler.DroneHandler, handlerWS *wshandler.DroneHandlerWS) {
 	mux.HandleFunc("POST /drone/goto", handler.Goto)
 	mux.HandleFunc("POST /drone/mission", handler.Mission)
+	mux.HandleFunc("DELETE /drone/mission", handler.StopMission)
 
 	mux.HandleFunc("/drone/position", handlerWS.DronePosHandle)
 	mux.HandleFunc("/drone/mission/status", handlerWS.DroneMissionHandle)
